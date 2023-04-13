97380f5c166255337a5e51fc28fb2a209ad345d1
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-04-13 20:13:40 +0530

    Initial Commit

---
+++ Frontend/pages/_app.jsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+import React from "react";
+import { DAppProvider } from "@usedapp/core";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <React.StrictMode>
+      <DAppProvider config={{}}>
+        <Component {...pageProps} />
+      </DAppProvider>
+    </React.StrictMode>
+  );
+}
+
+export default MyApp;
